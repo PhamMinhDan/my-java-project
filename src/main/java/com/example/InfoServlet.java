3ff18b705a1938b3d2309834d1541b87e4a62e51
Pham Minh Dan <phamdanngu937@gmail.com>
2025-06-24 17:50:29 +0700

    update

--- src/main/java/com/example/InfoServlet.java
+++ src/main/java/com/example/InfoServlet.java
@@ -1,9 +1,13 @@
 package com.example;
 
-import java.io.*;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/info")
 public class InfoServlet extends HttpServlet {
@@ -13,9 +17,8 @@ public class InfoServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<h2>Thông tin nhóm:</h2>");
         out.println("<ul>");
-        out.println("<li>Nguyễn Văn A - MSSV: 123</li>");
-        out.println("<li>Trần Thị B - MSSV: 456</li>");
-        out.println("<li>Lê Văn C - MSSV: 789</li>");
+        out.println("<li>DangTrongLuong - MSSV: BIT230250</li>");
+        out.println("<li>PhamMinhDan - MSSV: BIT230074</li>");
         out.println("</ul>");
     }
 }
