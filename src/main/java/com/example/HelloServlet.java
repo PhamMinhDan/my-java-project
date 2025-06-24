b296d52c1f21ce6938f368009386b29b8267b6b9
Pham Minh Dan <phamdanngu937@gmail.com>
2025-06-24 17:35:06 +0700

    Khởi tạo dự án my-java-project sạch

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("/hello9")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<h1>Hello, World, I am a servlet,chú ý nhánh của ---feature hello-24.06.2025!</h1>");
+
+    }
+}
